1f29cddf0fc024bbd64e335709355d176429523e
Milyn <milyng.dsilva@gmail.com>
2021-11-15 15:01:57 +0530

    implemented a simple truthy falsy program

--- Truthy-And-Falsy-Values.js
+++ Truthy-And-Falsy-Values.js
@@ -1,4 +1,12 @@
 //A falsy value is a value that is considered false when encountered in a Boolean context.
-//List of JS Falsy values
+//List of JS Falsy values.
 //false , 0 , -0 , 0n (bigint zero) , " " , '', ` `, null , undefined , NaN
+
+function TruthyOrFalsy(Val) {
+    return Val ? "truthy" : "falsy"; 
+}
+let Input = 1;
+console.log(TruthyOrFalsy(Input));
+
+
 
