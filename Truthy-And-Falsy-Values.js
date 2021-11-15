866cf54acdfd82f0387764679c8597ea8e550b2d
Milyn <milyng.dsilva@gmail.com>
2021-11-15 12:18:07 +0530

    Added defination

---
+++ Truthy-And-Falsy-Values.js
@@ -0,0 +1,4 @@
+//A falsy value is a value that is considered false when encountered in a Boolean context.
+//List of JS Falsy values
+//false , 0 , -0 , 0n (bigint zero) , " " , '', ` `, null , undefined , NaN
+
