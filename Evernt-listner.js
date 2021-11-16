cf556246a40b21c93fdba3bc81aa4b46b73089ea
Milyn <milyng.dsilva@gmail.com>
2021-11-16 13:32:43 +0530

    event listner

---
+++ Evernt-listner.js
@@ -0,0 +1,10 @@
+//<button id="myBtn">Try it</button>
+
+//
+document.getElementById("myBtn").addEventListener("click", myFunction);
+
+function myFunction() {
+  alert ("Hello World!");
+}
+
+//An event listener is a procedure or function in a computer program that waits for an event to occur. Examples of an event are the user clicking or moving the mouse, pressing
